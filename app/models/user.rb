0ffe85113e3ec3d9228b5c12e30f03c27ff6ca79
Jake Glasson <jakeiglasson@gmail.com>
2020-05-15 20:30:06 +1000

    generate devise user, then migrated
    Had to change the way devise created the email column inside its migration. Because email already existed I changed t.string email: to t.change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   enum seller: {false: 0, true: 1, requested: 2}, _prefix: :seller
   enum admin: {false: 0, true: 1}, _prefix: :admin
 
