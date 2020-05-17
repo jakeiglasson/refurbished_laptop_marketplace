d39e7ec751619723e10cc275b7383f52ffd7a333
Jake Glasson <jakeiglasson@gmail.com>
2020-05-17 14:51:56 +1000

    fixed admin column in users
    wasn't able to set admin due admin being set to enum in users model, removed the enum and ran a migration to remove admin then re-add as a Boolean with a default value of 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   enum seller: {false: 0, true: 1, requested: 2}, _prefix: :seller
-  enum admin: {false: 0, true: 1}, _prefix: :admin
 
   has_many :laptops, dependent: :destroy
   has_many :laptop_orders
-
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_100647) do
+ActiveRecord::Schema.define(version: 2020_05_17_034238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,12 +95,12 @@ ActiveRecord::Schema.define(version: 2020_05_15_100647) do
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "admin", default: 0
     t.integer "seller", default: 0
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
